9d01d0706396e73f991c056008bda59092088284
maraheters <blr.vladdd@gmail.com>
2025-04-30 14:01:23 +0300

    feat(exception): add exception handler filter and autoconfiguration

---
+++ common-authentication/src/main/java/com/sparkplug/commonauthentication/config/CommonAuthenticationAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.sparkplug.commonauthentication.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.sparkplug.commonauthentication")
+public class CommonAuthenticationAutoConfiguration {
+}
